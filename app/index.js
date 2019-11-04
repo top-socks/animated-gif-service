90a1cd0b11d1c79328dc8fcc99b50ab4cc868458
Steve Winton <stevewinton@gmail.com>
2019-11-04 15:17:27 -0600

    Add new gifme service

--- app/index.js
+++ app/index.js
@@ -9,6 +9,13 @@ app.get('/', async (request, response) => {
   response.send('Hello, 世界');
 });
 
+app.get('/gifme/:gif', async (request, response) => {
+  const { contentType, content } = await fetchImage(`https://bukk.it/${request.params.gif}`);
+
+  response.set('Content-Type', contentType);
+  response.send(content);
+});
+
 const listener = app.listen(process.env.PORT || 8000, process.env.HOST || '0.0.0.0', () => {
   logger.info(`Your app is listening on port ${listener.address().port}`);
 });
