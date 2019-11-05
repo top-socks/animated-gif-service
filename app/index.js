39e9881f103f04bc53888174c909bc3464cc0eac
Steve Winton <stevewinton@gmail.com>
2019-11-05 14:53:29 -0600

    Add new animated GIF service
    ![](https://bukk.it/bravery.gif)

--- app/index.js
+++ app/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 
 const logger = require('./lib/logger');
+const fetchImage = require('./lib/fetch-image');
 
 const app = express();
 
 app.get('/', async (request, response) => {
   response.send('Hello, 世界');
+});
+
+app.get('/gifme/:gif', async (request, response) => {
+  const { contentType, content } = await fetchImage(`https://bukk.it/${request.params.gif}`);
+
+  response.set('Content-Type', contentType);
+  response.send(content);
 });
 
 const listener = app.listen(process.env.PORT || 8000, process.env.HOST || '0.0.0.0', () => {
